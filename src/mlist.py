6adc01eebc6d57b7240330e6c296cfd2a024a78f
Fugante <ignisacedia@hotmail.com>
2024-08-01 13:13:37 -0600

    add input validation to Mlist class methods

--- src/mlist.py
+++ src/mlist.py
@@ -1,36 +1,41 @@
 from __future__ import annotations
+from inspect import signature
+from functools import partial
 from typing import Callable, Iterable, Awaitable
 
 from .categories import Monoid, Monad, Foldable
 
 
-class MList[A](list, Monoid, Monad, Foldable):
-    def __init__(self, iterable: Iterable[A] = None) -> None:
+class Mlist[A](list, Monoid, Monad, Foldable):
+    def __init__(self, iterable: Iterable[A] | None = None) -> None:
         super().__init__(iterable if iterable is not None else [])
 
-    def __add_(self: MList[A], m: MList[A]) -> MList[A]:
-        return self.mappend(m)
-
     @classmethod
-    def mempty(cls) -> MList[A]:
-        return MList([])
+    def mempty(cls) -> Mlist[A]:
+        return Mlist([])
 
-    def mappend(self: MList[A], m: MList[A]) -> MList[A]:
-        return self + m
+    def map[A, B](self: Mlist[A], f: Callable[A, B]) -> Mlist[B]:
+        params = signature(f).parameters
+        # TODO: raise error if number of parameters is 0
+        if len(params) == 1:
+            return Mlist(f(a) for a in self)
+        return Mlist(partial(f, a) for a in self)
 
-    def map[A, B](self: MList[A], f: Callable[A, B]) -> MList[B]:
-        return [f(a) for a in self]
+    async def amap[A, B](self: Mlist[A], f: Callable[A, Awaitable[B]]) -> Mlist[B]:
+        params = signature(f).parameters
+        # TODO: raise error if number of parameters is 0
+        if len(params) == 1:
+            return Mlist(await f(a) for a in self)
+        return Mlist(partial(f, a) for a in self)
 
-    async def amap[A, B](self: MList[A], f: Callable[A, Awaitable[B]]) -> MList[B]:
-        return [await f(a) for a in self]
+    def apply[A, B](self: Mlist[A], fs: Mlist[Callable[A, B]]) -> Mlist[B]:
+        # TODO: validate all functions have same signature
+        return Mlist(f(a) for f in fs for a in self)
 
-    def apply[A, B](self: MList[A], fs: MList[Callable[A, B]]) -> MList[B]:
-        return MList(f(a) for f in fs for a in self)
+    def bind[A, B](self: Mlist[A], f: Callable[A, Mlist[B]]) -> Mlist[B]:
+        return Mlist(b for lb in (f(a) for a in self) for b in lb)
 
-    def bind[A, B](self: MList[A], f: Callable[A, MList[B]]) -> MList[B]:
-        return MList(b for lb in (f(a) for a in self) for b in lb)
-
-    def fold[A, B](self: MList[A], f: Callable[[A, B], B], b: B) -> B:
+    def fold[A, B](self: Mlist[A], f: Callable[[A, B], B], b: B) -> B:
         for a in self:
             b = f(a, b)
         return b
