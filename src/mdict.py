c50a428662d78513e1c5ba8324694e1a1ec37eb1
Fugante <ignisacedia@hotmail.com>
2024-08-01 13:22:33 -0600

    add input validation to Mdict's methods

--- src/mdict.py
+++ src/mdict.py
@@ -1,42 +1,50 @@
 from __future__ import annotations
+from inspect import signature
+from functools import partial
 from typing import Callable
 
 from .categories import Monoid, Monad, Foldable
 
 
-class MDict[T, A](dict, Monoid, Monad, Foldable):
+class Mdict[T, A](dict, Monoid, Monad, Foldable):
     def __init__(self, value: dict[T, A] = None) -> None:
         super().__init__(value if value is not None else {})
 
-    def __add__(self: MDict[T, A], m: MDict[T, A]) -> MDict[T, A]:
-        return self.mappend(m)
+    def __add__(self: Mdict[T, A], m: Mdict[T, A]) -> Mdict[T, A]:
+        return self.append(m)
 
     @classmethod
-    def mempty(cls) -> MDict[T, A]:
+    def mempty(cls) -> Mdict[T, A]:
         return cls()
 
-    def append(self: MDict[T, A], m: MDict[T, A]) -> MDict[T, A]:
+    def append(self: Mdict[T, A], m: Mdict[T, A]) -> Mdict[T, A]:
         return self.update(m)
 
-    def map[A, B](self: MDict[T, A], f: Callable[A, B]) -> MDict[T, B]:
-        return MDict({t: f(a) for (t, a) in self.items()})
+    def map[A, B](self: Mdict[T, A], f: Callable[A, B]) -> Mdict[T, B]:
+        params = signature(f).parameters
+        # TODO: raise error if parameters lenght is 0
+        if len(params) == 1:
+            return Mdict({t: f(a) for (t, a) in self.items()})
+        return Mdict({t: partial(f, a) for (t, a) in self.items()})
 
     def apply[A, B](
-        self: MDict[Monoid, A], f: MDict[Monoid, Callable[A, B]]
-    ) -> MDict[Monoid, B]:
-        d = MDict()
+        self: Mdict[Monoid, A], f: Mdict[Monoid, Callable[A, B]]
+    ) -> Mdict[Monoid, B]:
+        # TODO: validate all functions have the same signature
+        # TODO: raise error if self and f are not Monoids
+        d = Mdict()
         for m1, a in self.items():
             for m2, f in f.items():
                 d[m1.mappend(m2)] = f(a)
         return d
 
-    def bind[A, B](self: MDict[T, A], f: Callable[A, MDict[T, B]]) -> MDict[T, B]:
-        d1 = MDict()
+    def bind[A, B](self: Mdict[T, A], f: Callable[A, Mdict[T, B]]) -> Mdict[T, B]:
+        d1 = Mdict()
         for a in self.values():
             d1.update(f(a))
         return d1
 
-    def fold[A, B](self: MDict[T, A], f: Callable[A, B, B], b: B) -> B:
+    def fold[A, B](self: Mdict[T, A], f: Callable[[A, B], B], b: B) -> B:
         for a in self.values():
             b = f(a, b)
         return b
